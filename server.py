dee54fcd4f32d9ecb92912b8438314e85423848e
Wesley Dalbock <dalbockwesley@gmail.com>
2025-05-08 16:15:22 +0800

    work in progress

--- server.py
+++ server.py
@@ -6,25 +6,48 @@ from utils import *
 
 HOST = '127.0.0.1'
 PORT = 5000
-NUM_PLAYERS = 2
+CLIENT_LIMIT = 5
 
 players = []
+players_lock = threading.Lock()
 
 class Player:
     def __init__(self, conn, addr):
         self.conn = conn
         self.addr = addr
 
+def remove_player(player):
+    with players_lock:
+        if player in players:
+            try:
+                player.conn.close()
+            except:
+                pass
+            players.remove(player)
+            print(f"[INFO] Player {player.addr} has been removed.")
+            send_queue_pos()
+
+def send_queue_pos():
+    num_players = len(players)
+    if num_players == 1:
+        send_package(players[0].conn, MessageTypes.WAITING, "Please wait whilst we find you an opponent.")
+        return
+    for i in range(2, num_players): #Skip 1st and 2nd player as they are in the game already.
+        player = players[i]
+        send_package(player.conn, MessageTypes.S_MESSAGE, f"You are in position ({i-1}) of the queue to play battleship.")
+        send_package(player.conn, MessageTypes.WAITING, "Waiting...")
+
 def connect_with_clients(s: socket.socket):
     s.bind((HOST, PORT))
-    s.listen(NUM_PLAYERS)
+    s.listen(CLIENT_LIMIT)
     while True:
-        while len(players) < NUM_PLAYERS:
+        while len(players) < CLIENT_LIMIT:
             conn, addr = s.accept()
-            print(f"[INFO] Client {len(players) + 1} connected from {addr}")
-            players.append(Player(conn, addr))
-            if len(players) < NUM_PLAYERS:
-                send_package(conn, MessageTypes.WAITING, "Waiting for an opponent to connect...")
+            print(f"[INFO] Client connected from {addr}")
+            with players_lock:
+                players.append(Player(conn, addr))
+            send_queue_pos()
+        time.sleep(1)
 
 def play_game(p1: Player, p2: Player):
     try:
@@ -45,33 +68,27 @@ def play_game(p1: Player, p2: Player):
         else:
             print("[INFO] At least one player has declined. Closing the appropriate connections.")
             if response1 != "YES":
-                p1.conn.close()
-                players.remove(p1)
+                remove_player(p1)
             if response2 != "YES":
-                p2.conn.close()
-                players.remove(p2)
-            for player in players:
-                send_package(player.conn, MessageTypes.S_MESSAGE, "Please wait whilst we find another player...")
-
+                remove_player(p2)
+            
     except Exception as e:
         print(f"[ERROR] Exception during game: {e}")
         print("[INFO] Closing connections...")
-        p1.conn.close()
-        p2.conn.close()
-        if p1 in players: players.remove(p1)
-        if p2 in players: players.remove(p2)
+        remove_player(p1)
+        remove_player(p2)
 
 def main():
     print(f"[INFO] Server listening on {HOST}:{PORT}")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
         client_connecting_thread = threading.Thread(target=connect_with_clients, args=(s,), daemon=True)
         client_connecting_thread.start()
 
         try:
             while True:
                 if len(players) >= 2:
+                    print(players)
                     p1 = players[0]
                     p2 = players[1]
 
